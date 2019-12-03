6694b0e33c9f5b37e9fbfe5b56572d4750aa130e
liu <1373788543@qq.com>
2019-12-03 12:13:18 +0800

    循环&递归

---
+++ RectCover.java
@@ -0,0 +1,16 @@
+package Recursive_Loop;
+
+public class RectCover {
+    public int RectCover(int target) {
+        if (target < 1) {
+            return 0;
+        }
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        return RectCover(target - 1) + RectCover(target - 2);
+    }
+}
